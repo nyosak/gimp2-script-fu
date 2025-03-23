f2b079ebe3703b3b57c946ab5c4d44ad23b6d0b6
nosaki <nyosakey@gmail.com>
2025-03-23 14:52:00 +0900

    fix bugs and add example1 tree snippet

--- snippets/tree.scm
+++ snippets/tree.scm
@@ -3,7 +3,7 @@
 ; copyright 2025, hanagai
 ;
 ; tree.scm
-; version: March 21, 2025
+; version: March 23, 2025
 ;
 ; copy and paste the code to your script
 ; or load this file by (load "/path/to/tree.scm")
@@ -210,13 +210,27 @@
 ; node-attribute-keys: list (expected) -> list
 ; Returns a list of key in attributes of the specified node
 (define (node-attribute-keys node)
-  (map get-attribute-key (node-attributes node))
+  (map get-attribute-key (get-node-body (node-attributes node)))
 )
 
 ; node-attribute-values: list (expected) -> list
 ; Returns a list of values in attributes of the specified node
 (define (node-attribute-values node)
-  (map get-attribute-value (node-attributes node))
+  (map get-attribute-value (get-node-body (node-attributes node)))
+)
+
+; node-has-any-attribute?: list (expected) . optional strings -> boolean
+; check if the node has attribute of specified keys
+; Return #t if the node has one of keys as attributes, otherwise #f
+; Return #f if no keys are specified
+(define (node-has-any-attribute? node . keys)
+  (if (eq? () keys )
+    #f
+    (if (node-has-attribute? node (car keys))
+      #t
+      (apply node-has-any-attribute? (cons node (cdr keys)))
+    )
+  )
 )
 
 ; node-has-attribute?: list (expected) . optional strings -> boolean
@@ -225,16 +239,16 @@
 ; Return #t if no keys are specified
 (define (node-has-attribute? node . keys)
   (let
-    ((attr (node-attributes node)))
+    ((attr (get-node-body (node-attributes node))))
     (foldr (lambda (x key) (and x (attribute-has-key? attr key))) #t keys)
   )
 )
 
-; node-get-attribute: list (expected) string -> pair (expected)
-; Returns an attribute pair from the node by the specified key
+; node-get-attribute: list (expected) string -> string/integer (expected)
+; Returns an attribute value from the node by the specified key
 ; Returns () if the key is not found
 (define (node-get-attribute node key)
-  (attribute-search-key (node-attributes node) key)
+  (attribute-search-key (get-node-body (node-attributes node)) key)
 )
 
 ; attribute-has-key?: list (expected) string -> boolean
@@ -328,6 +342,295 @@
   (apply-node func-l func-n func-a func-h func-else node opts)
 )
 
+; execute-example1: execute example1
+(define (execute-example1 image project version arguments hierarchy node)
+  (define (make-opts depth image project version arguments hierarchy)
+    (list depth image project version arguments hierarchy)
+  )
+
+  (define (duplicate-image image)
+    (show-info "INFO: duplicate current image")
+    (show-info "TODO: handle GIMP")
+    (list "new-image" "new-display")  ; TODO: return image info
+  )
+
+  (define (discard-image display)
+    (show-info "INFO: delete display and image")
+    (show-info "TODO: handle GIMP")
+    "INFO: delete display and image"
+  )
+
+  (let
+    ((return-value ()))
+    (show-info "BEGIN:")
+    (let*
+      (
+        ; a copy is used as a base working image
+        (working (duplicate-image image))
+        (new-image (car working))
+        (new-display (cadr working))
+        (depth 0)
+        (opts (make-opts depth new-image project version arguments hierarchy))
+      )
+      (show-info "INFO: image is duplicated for working")
+      (show-info new-image new-display)
+      (show-info node opts)
+
+      (set! return-value (example1 node opts))
+      (discard-image new-display)
+
+      (show-info "INFO: working image was discarded")
+    )
+    (show-info "DONE:")
+    return-value
+  )
+)
+
+; example1: for GIMP
+; depth 1: modify image to fit build and shape
+; depth 2: update size in opts
+;          export scaled image as webp
+(define (example1 node opts)
+
+  ; node functions
+
+  (define (func-l node opts)
+    (show-info "--- func-l ---" node opts)
+    (show-info "INFO: nodes at depth:" (get-depth opts))
+    (map
+      (lambda (n) (example1 n (depth+ opts)))
+      (nodes-siblings node)
+    )
+  )
+
+  (define (func-n node opts)
+    (show-info "--- func-n ---" node opts)
+    (show-info "INFO: node at depth:" (get-depth opts))
+    (list
+      (example1 (node-attributes node) opts)
+      (example1 (node-body node) opts)
+    )
+  )
+
+  (define (func-a node opts)
+    (show-info "--- func-a ---" node opts)
+    (let
+      (
+        (depth (get-depth opts))
+      )
+      (show-info "INFO: attributes at depth:" depth)
+      (case depth
+        ((1) (func-a-1 node opts))
+        ((2) (func-a-2 node opts))
+        (else '("Too deep for a human"))
+      )
+    )
+  )
+
+  (define (func-a-1 node opts)
+    (show-info "--- func-a-1 ---" node opts)
+    (let*
+      (
+        (attr (get-node-body node))
+        (image (get-image opts))
+      )
+      (show-info "INFO: modify image to fit specified build and shape")
+      (list
+        (if (attribute-has-key? attr "build")
+          (handle-build image (attribute-search-key attr "build"))
+        )
+        (if (attribute-has-key? attr "shape")
+          (handle-shape image (attribute-search-key attr "shape"))
+        )
+      )
+    )
+  )
+
+  (define (func-a-2 node opts)
+    (show-info "--- func-a-2 ---" node opts)
+    (let*
+      (
+        (attr (get-node-body node))
+        (image (get-image opts))
+      )
+      (show-info "INFO: set size to local variable")
+      (list
+        (if (attribute-has-key? attr "size")
+          (set! size (attribute-search-key attr "size"))
+          (handle-size image size)
+        )
+      )
+    )
+  )
+
+  (define (func-h node opts)
+    (show-info "--- func-h ---" node opts)
+    (let*
+      (
+        (image (get-image opts))
+        (project (get-project opts))
+        (version (get-version opts))
+        (arguments (get-arguments opts))
+        (hierarchy (get-hierarchy opts))
+        (new-hierarchy (merge-arguments hierarchy arguments project version))
+      )
+      (show-info "INFO: reflect arguments (project, version) into hierarchy")
+      (show-info hierarchy arguments project version new-hierarchy)
+      (export-scaled-webp image size new-hierarchy)
+    )
+  )
+
+  (define (func-else node opts)
+    (show-info "--- func-else ---" node opts)
+    (if (eqv? empty-list node)
+      ()
+      (begin
+        (show-info "ERROR: Unknown contens found")
+        "ERROR: Unknown contens found"
+      )
+    )
+  )
+
+  ; gimp
+
+  (define (duplicate-image image)
+    (show-info "INFO: duplicate current image")
+    (show-info "TODO: handle GIMP")
+    (list "new-image" "new-display")  ; TODO: return image info
+  )
+
+  (define (discard-image display)
+    (show-info "INFO: delete display and image")
+    (show-info "TODO: handle GIMP")
+    "INFO: delete display and image"
+  )
+
+  (define (handle-build image build)
+    (show-info "--- handle-build ---" image build)
+    (show-info "INFO: change layer visibility by build")
+    (show-info "TODO: handle GIMP")
+    (show-info "TODO: remember flush display")
+    "INFO: change layer visibility by build"
+  )
+
+  (define (handle-shape image shape)
+    (show-info "--- handle-shape ---" image shape)
+    (show-info "INFO: change layer visibility by shape")
+    (show-info "TODO: handle GIMP")
+    (show-info "TODO: remember flush display")
+    "INFO: change layer visibility by shape"
+  )
+
+  (define (handle-size image arg_size)
+    (show-info "--- handle-size ---" image arg_size)
+    (show-info "INFO: nothing here for GIMP")
+    "INFO: nothing here for GIMP"
+  )
+
+  (define (export-scaled-webp image arg_size hierarchy)
+    (show-info "--- export-scaled-webp ---" image arg_size hierarchy)
+    (show-info "INFO: scale image and export as webp")
+    (show-info "TODO: handle GIMP")
+    "INFO: scale image and export as webp"
+  )
+
+  ; helper
+
+  (define (merge-arguments hierarchy arguments project version)
+    (if (eq? () hierarchy)
+      ()
+      (let*
+        (
+          (h (car hierarchy))
+          (a (car arguments))
+          (new-h
+            (if a
+              (if (string=? "project" a)
+                project
+                (if (string=? "version" a)
+                  version
+                  h
+                )
+              )
+              h
+            )
+          )
+        )
+        (cons new-h (merge-arguments (cdr hierarchy) (cdr arguments) project version))
+      )
+    )
+  )
+
+  ; opts
+
+  (define (get-depth opts)
+    (car opts)
+  )
+
+  (define (get-after-depth opts)
+    (cdr opts)
+  )
+
+  (define (get-image opts)
+    (cadr opts)
+  )
+
+  (define (get-after-image opts)
+    (cddr opts)
+  )
+
+  (define (get-project opts)
+    (caddr opts)
+  )
+
+  (define (get-version opts)
+    (cadddr opts)
+  )
+
+  (define (get-arguments opts)
+    (car (cddddr opts))
+  )
+
+  (define (get-hierarchy opts)
+    (cadr (cddddr opts))
+  )
+
+  (define (make-opts depth image project version arguments hierarchy)
+    (list depth image project version arguments hierarchy)
+  )
+
+  (define (depth+ opts)
+    (cons (+ 1 (get-depth opts)) (get-after-depth opts))
+  )
+
+  (define (with-image opts image)
+    (cons (get-depth opts) (cons image (get-after-image opts)))
+  )
+
+  (define (with-image-depth+ opts image)
+    (cons (+ 1 (get-depth opts)) (cons image (get-after-image opts)))
+  )
+
+  ; internode variable
+
+  ; defines size to communicate between leaves directly
+  (define size 0)
+  ; Attribute gives size to hierarchy.
+  ; It also can be done using paired return value,
+  ; (return as node . additional return as attributes including size),
+  ; because the both have same parent node.
+  ; Maybe, in this example, using wide scope variable seems simpler.
+  ; At multi threading, such a variable cannot work,
+  ; so requires latter solution of structured return value.
+  ; On TinyScheme we can forget that.
+  ;
+  ; In the case where the entire return value is not required,
+  ; using node return value to pass size or something is best.
+
+  ; begin this node
+
+  (apply-node func-l func-n func-a func-h func-else node opts)
+)
 
 ; test
 (load "./test.scm")
@@ -335,6 +638,14 @@
 (define (tree-submit-test)
   (let*
     (
+
+      (hierarchy 
+        '("user_home" "studio_home" "project" "version" "src" "build" "res" "mipmap" "icon_name")
+      )
+
+      (arguments 
+        '(#f #f "project" "version" #f #f #f #f #f)
+      )
 
       (icon-list
         '
@@ -369,92 +680,6 @@
                 )
                 (
                   #\H
-                  "/home/kuro"
-                  "tmp/art_work"
-                  "p"
-                  "v"
-                  "app/src"
-                  "main"
-                  "res"
-                  "mipmap-xhdpi"
-                  "ic_launcher.webp"
-                )
-              )
-            )
-          )
-        )
-      )
-
-      (icon-list-vector
-        '
-        (list
-          (vector
-            (vector
-              (
-                "build"
-              .
-                "main"
-              )
-              (
-                "shape"
-              .
-                "square"
-              )
-            )
-            (list
-              (vector
-                (vector
-                  (
-                    "size"
-                  .
-                    96
-                  )
-                )
-                (vector
-                  "/home/kuro"
-                  "tmp/art_work"
-                  "p"
-                  "v"
-                  "app/src"
-                  "main"
-                  "res"
-                  "mipmap-xhdpi"
-                  "ic_launcher.webp"
-                )
-              )
-            )
-          )
-        )
-      )
-
-      (icon-list-cons
-        '
-        (
-          (
-            (
-              (
-                "build"
-              .
-                "main"
-              )
-              (
-                "shape"
-              .
-                "square"
-              )
-            )
-          .
-            (
-              (
-                (
-                  (
-                    "size"
-                  .
-                    96
-                  )
-                )
-              .
-                (
                   "/home/kuro"
                   "tmp/art_work"
                   "p"
@@ -609,10 +834,170 @@
           )
 
           (
+            attributes? ; function
+            #t  ; expected return
+            ,(list-ref (list-ref icon-list 1) 1) ; arguments from here
+          )
+
+          (
+            node-attributes ; function
+            (#\A ("build" . "main") ("shape" . "square"))  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+          )
+
+          (
+            node-attribute-keys ; function
+            ("build" "shape") ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+          )
+
+          (
+            node-attribute-values ; function
+            ("main" "square") ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+          )
+
+          (
+            node-get-attribute ; function
+            "square" ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "shape"
+          )
+          (
+            node-get-attribute ; function
+            () ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "size"
+          )
+
+          (
+            node-has-any-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+          )
+          (
+            node-has-any-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+            "shape"
+          )
+          (
+            node-has-any-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "shape"
+          )
+          (
+            node-has-any-attribute? ; function
+            #f  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "size"
+          )
+          (
+            node-has-any-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+            "shape"
+            "size"
+          )
+          (
+            node-has-any-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "size"
+            "shape"
+          )
+          (
+            node-has-any-attribute? ; function
+            #f  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "size"
+            "not_key"
+          )
+          (
+            node-has-any-attribute? ; function
+            #f  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+          )
+
+          (
+            node-has-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+          )
+          (
+            node-has-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+            "shape"
+          )
+          (
+            node-has-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "shape"
+          )
+          (
+            node-has-attribute? ; function
+            #f  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "size"
+          )
+          (
+            node-has-attribute? ; function
+            #f  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "build"
+            "shape"
+            "size"
+          )
+          (
+            node-has-attribute? ; function
+            #t  ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+          )
+
+          (
+            node-get-attribute ; function
+            "square" ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "shape"
+          )
+
+          (
+            node-get-attribute ; function
+            () ; expected return
+            ,(list-ref icon-list 1) ; arguments from here
+            "no_key"
+          )
+
+          (
             traverse-tree ; function
-            "expects fail"  ; expected return
+            ((((#\A ("build" . "main") ("shape" . "square")) (1)) ((((#\A ("size" . 96)) (2)) ((#\H "/home/kuro" "tmp/art_work" "p" "v" "app/src" "main" "res" "mipmap-xhdpi" "ic_launcher.webp") (2))))))  ; expected return
             ,icon-list ; arguments from here
             (0)
+          )
+
+          ; (
+          ;   example1 ; function
+          ;   "something" ; expected return
+          ;   ,icon-list ; arguments from here
+          ;   (0 "image" "new_project" "new_version" ,arguments ,hierarchy)
+          ; )
+          (
+            execute-example1 ; function
+            "something" ; expected return
+            "image" ; arguments from here
+            "new-project"
+            "new-version"
+            ,arguments
+            ,hierarchy
+            ,icon-list
           )
 
         )
